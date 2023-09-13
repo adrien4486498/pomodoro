e435767aabeeb4574d47983fb852f0a49497a234
Adrien Mussier <adrienmussier11@gmail.com>
2023-09-13 22:29:07 +0200

    le timer alterne entre les temps de pause et de travail

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const boxElement = document.querySelector('.box');
 
 let timerInterval;
 let minutes = 0;
-let seconds = 10;
-let isWorking = false; // Indicateur pour suivre si le minuteur est en mode travail ou pause
+let seconds = 15;
+let isWorking = true; // Indicateur pour suivre si le minuteur est en mode travail ou pause
 
 // Fonction pour mettre à jour l'affichage du minuteur
 function updateTimer() {
@@ -16,38 +16,55 @@ function updateTimer() {
 
 // Fonction pour démarrer ou annuler le minuteur
 function startTimer() {
-    if (isWorking) {
-        clearInterval(timerInterval);
-        isWorking = false;
-        startButton.innerHTML = '<i class="fa-solid fa-play"></i>'; // Changez le texte ou l'icône du bouton pour indiquer le démarrage
-        boxElement.textContent = 'Pause';
+    if (timerInterval) {
+      clearInterval(timerInterval);
+      timerInterval = null;
+      startButton.innerHTML = '<i class="fa-solid fa-play"></i>';
     } else {
+      startButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+      if (minutes === 0 && seconds === 0) {
+        // Si le minuteur est en mode travail ou pause et se termine, basculez automatiquement en mode travail
+        minutes = 0;
+        seconds = 15;
+        boxElement.textContent = 'Travail';
         isWorking = true;
-        startButton.innerHTML = '<i class="fa-solid fa-pause"></i>'; // Changez le texte ou l'icône du bouton pour indiquer l'arrêt
-        if(minutes === 0 && seconds === 0){
-            minutes = 25;
-            seconds = 0;
+      } else if (isWorking) {
+        boxElement.textContent = 'Travail';
+      } else {
+        boxElement.textContent = 'Pause';
+      }
+      timerInterval = setInterval(function () {
+        if (minutes === 0 && seconds === 0) {
+          clearInterval(timerInterval);
+          timerInterval = null;
+          startButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+          if (isWorking) {
+            // Si le minuteur est en mode travail, basculez automatiquement en mode pause
+            minutes = 0;
+            seconds = 5;
             boxElement.textContent = 'Pause';
-        } else {
+            isWorking = false;
+            startTimer(); // Démarrez automatiquement le minuteur de pause
+          } else {
+            // Si le minuteur est en mode pause, basculez automatiquement en mode travail
+            minutes = 0;
+            seconds = 15;
             boxElement.textContent = 'Travail';
+            isWorking = true;
+            startTimer(); // Démarrez automatiquement le minuteur de travail
+          }
+        } else {
+          if (seconds === 0) {
+            minutes--;
+            seconds = 59;
+          } else {
+            seconds--;
+          }
+          updateTimer();
         }
-        timerInterval = setInterval(function () {
-            if (minutes === 0 && seconds === 0) {
-                clearInterval(timerInterval);
-                isWorking = !isWorking;
-                startTimer();
-            } else {
-                if (seconds === 0) {
-                    minutes--;
-                    seconds = 59;
-                } else {
-                    seconds--;
-                }
-                updateTimer();
-            }
-        }, 1000);
+      }, 1000);
     }
-}
+  }
 
 // Écouteur d'événement pour le bouton de démarrage
 startButton.addEventListener('click', startTimer);
