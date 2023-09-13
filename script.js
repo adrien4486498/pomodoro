a20cda443962a811332c830ff940521a03757d05
Adrien Mussier <adrienmussier11@gmail.com>
2023-09-13 18:54:53 +0200

    le timer de pause se lance dès que le timer de travail est terminé

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
 // Récupérez les éléments du DOM
 const timerElement = document.getElementById('timer');
 const startButton = document.getElementById('button');
+const boxElement = document.querySelector('.box');
 
 
 let timerInterval;
-let minutes = 25;
-let seconds = 0;
-let isRunning = false;
+let minutes = 0;
+let seconds = 10;
+let isWorking = false; // Indicateur pour suivre si le minuteur est en mode travail ou pause
 
 // Fonction pour mettre à jour l'affichage du minuteur
 function updateTimer() {
-  timerElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    timerElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
 // Fonction pour démarrer ou annuler le minuteur
 function startTimer() {
-    if (isRunning) {
-      clearInterval(timerInterval);
-      isRunning = false;
-      startButton.innerHTML = '<i class="fa-solid fa-play"></i>'; // Changez le texte ou l'icône du bouton pour indiquer le démarrage
+    if (isWorking) {
+        clearInterval(timerInterval);
+        isWorking = false;
+        startButton.innerHTML = '<i class="fa-solid fa-play"></i>'; // Changez le texte ou l'icône du bouton pour indiquer le démarrage
+        boxElement.textContent = 'Pause';
     } else {
-      isRunning = true;
-      startButton.innerHTML = '<i class="fa-solid fa-pause"></i>'; // Changez le texte ou l'icône du bouton pour indiquer l'arrêt
-      timerInterval = setInterval(function () {
-        if (minutes === 0 && seconds === 0) {
-          clearInterval(timerInterval);
-          isRunning = false;
-          startButton.innerHTML = '<i class="fa-solid fa-play"></i>'; // Changez le texte ou l'icône du bouton pour indiquer le démarrage
-          // Redémarrez automatiquement le minuteur à la fin du temps (25 minutes)
-          minutes = 25;
-          seconds = 0;
-          updateTimer();
+        isWorking = true;
+        startButton.innerHTML = '<i class="fa-solid fa-pause"></i>'; // Changez le texte ou l'icône du bouton pour indiquer l'arrêt
+        if(minutes === 0 && seconds === 0){
+            minutes = 25;
+            seconds = 0;
+            boxElement.textContent = 'Pause';
         } else {
-          if (seconds === 0) {
-            minutes--;
-            seconds = 59;
-          } else {
-            seconds--;
-          }
-          updateTimer();
+            boxElement.textContent = 'Travail';
         }
-      }, 1000);
+        timerInterval = setInterval(function () {
+            if (minutes === 0 && seconds === 0) {
+                clearInterval(timerInterval);
+                isWorking = !isWorking;
+                startTimer();
+            } else {
+                if (seconds === 0) {
+                    minutes--;
+                    seconds = 59;
+                } else {
+                    seconds--;
+                }
+                updateTimer();
+            }
+        }, 1000);
     }
-  }
+}
 
 // Écouteur d'événement pour le bouton de démarrage
 startButton.addEventListener('click', startTimer);
